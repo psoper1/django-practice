c9ecdce242c8b16b49970d6cc8d1c36a353c232a
reeman9000 <preston.soper@student.awesomeinc.org>
2023-03-28 19:06:49 +0000

    added items to the database

--- heros/heroes/models.py
+++ heros/heroes/models.py
@@ -3,9 +3,21 @@ from django.db import models
 # Create your models here.
 class Hero(models.Model):
     name = models.CharField(max_length=50)
-    about_me = models.CharField(max_length=200)
+    about_me = models.CharField(max_length=200, null=True, blank=True)
     biography = models.CharField(max_length=400)
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return self.name
 
 class Ability(models.Model):
     ability_name = models.CharField(max_length=100)
     abilities = models.ManyToManyField('Hero')
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return self.ability_name
